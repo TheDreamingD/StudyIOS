bb197b4b46bc3c8adc9212b0644d4963b4ccbbf2
DreamingDragon <thedreamingd@gmail.com>
2022-11-07 18:32:50 +0900

    03까지 학습

---
+++ InflearnObjectiveC/First/First/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  First
+//
+//  Created by YONG KWON on 2022/11/07.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        int wheels = 4; // int, float, double, long, ... - primitive type
+        int seats = 2;
+        
+        NSLog(@"wheels : %i, seats : %i", wheels, seats);
+    }
+    return 0;
+}
