d762ee0b05f2616c260d53e3d70bbf69f66aeb67
YKWON <yongkwon@mappers.kr>
2022-11-11 14:53:00 +0900

    Inflearn Objective-C 42까지 학습

--- InflearnObjectiveC/DigitalFrame/DigitalFrame/ViewController.h
+++ InflearnObjectiveC/DigitalFrame/DigitalFrame/ViewController.h
@@ -8,7 +8,7 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
-
+@property (nonatomic, strong) IBOutlet UIImageView *imgView;
 
 @end
 
--- InflearnObjectiveC/DigitalFrame/DigitalFrame/ViewController.m
+++ InflearnObjectiveC/DigitalFrame/DigitalFrame/ViewController.m
@@ -12,6 +12,7 @@
 @end
 
 @implementation ViewController
+@synthesize imgView;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
