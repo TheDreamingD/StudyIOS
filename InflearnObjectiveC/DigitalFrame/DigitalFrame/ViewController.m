26c057aaccc0622361035f41f051a0b655c423e3
YKWON <yongkwon@mappers.kr>
2022-11-11 15:08:58 +0900

    Inflearn Objective-C 43까지 학습

--- InflearnObjectiveC/DigitalFrame/DigitalFrame/ViewController.m
+++ InflearnObjectiveC/DigitalFrame/DigitalFrame/ViewController.m
@@ -16,7 +16,24 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    NSArray *cuteImages = [[NSArray alloc] initWithObjects:
+                           [UIImage imageNamed:@"icon_infomation_camera_big.png"],
+                           [UIImage imageNamed:@"icon_infomation_camera.png"],
+                           [UIImage imageNamed:@"icon_infomation_place_big.png"],
+                           [UIImage imageNamed:@"icon_infomation_place.png"],
+                           [UIImage imageNamed:@"icon_infomation_road_big.png"],
+                           [UIImage imageNamed:@"icon_infomation_road.png"],
+                           [UIImage imageNamed:@"icon_photo_upload.png"],
+                           [UIImage imageNamed:@"image_del_png.png"],
+                           [UIImage imageNamed:@"information_img.png"],
+                           [UIImage imageNamed:@"location_check_day.png"],
+                           [UIImage imageNamed:@"location_check_night.png"],
+                           nil];
+    
+    imgView.animationImages = cuteImages;
+    imgView.animationDuration = 11.0;
+    [imgView startAnimating];
 }
 
 
