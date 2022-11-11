0ba0fb2636b412433f780535387502078c453805
YKWON <yongkwon@mappers.kr>
2022-11-11 16:36:37 +0900

    Inflearn Objective-C 49까지 학습

---
+++ InflearnObjectiveC/SignUpScreen/SignUpScreen/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  SignUpScreen
+//
+//  Created by YONG KWON on 2022/11/11.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ InflearnObjectiveC/SignUpScreen/SignUpScreen/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  SignUpScreen
+//
+//  Created by YONG KWON on 2022/11/11.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
