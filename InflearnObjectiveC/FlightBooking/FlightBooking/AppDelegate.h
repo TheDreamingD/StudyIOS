d06035dee9cb340a819b6cb4bd4bd15f3b51bb10
Dreaming Dragon <thedreamingd@gmail.com>
2022-11-17 10:31:56 +0900

    Inflearn Objective-C 77까지 학습

---
+++ InflearnObjectiveC/FlightBooking/FlightBooking/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  FlightBooking
+//
+//  Created by YONG KWON on 2022/11/16.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
